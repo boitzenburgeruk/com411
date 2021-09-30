d411ce4d0c76bb5088b51287b358d3f3fc76da7d
Angus <5boita75@solent.ac.uk>
2021-09-30 16:04:52 +0100

    Created a basic input/output program

--- basics/input/review.py
+++ basics/input/review.py
@@ -5,5 +5,5 @@ cake = "🎂"
 name = str(input("What is your name, human? "))
 age = int(input(f"How old are you, {name}? "))
 
-print(f"So you are {age} years old? Have a cake for every year I missed!")
+print(f"So you are {age} years old? Have a cake for every year you have existed!")
 print(cake*age)
