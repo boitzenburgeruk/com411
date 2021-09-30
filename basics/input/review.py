732de0120ddb8254f5295dcd0d97b2b19ba3736d
Angus <5boita75@solent.ac.uk>
2021-09-30 16:18:39 +0100

    Removed 'cake' variable, placed cake UTF character straight into a print command

--- basics/input/review.py
+++ basics/input/review.py
@@ -1,9 +1,8 @@
 # Program combines all programming techniques used in the files in the 'basics' folder
-
-cake = "🎂"
+# Techniques used: output, input, variables, operators, data types
 
 name = str(input("What is your name, human? "))
 age = int(input(f"How old are you, {name}? "))
 
 print(f"So you are {age} years old? Have a cake for every year you have existed!")
-print(cake*age)
+print("🎂"*age)
