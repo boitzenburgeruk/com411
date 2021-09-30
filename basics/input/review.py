119b751a57396f0ca0646889468c36087f7bd3a9
Angus <5boita75@solent.ac.uk>
2021-09-30 16:02:34 +0100

    Created a basic input/output program

---
+++ basics/input/review.py
@@ -0,0 +1,9 @@
+# Program combines all programming techniques used in the files in the 'basics' folder
+
+cake = "🎂"
+
+name = str(input("What is your name, human? "))
+age = int(input(f"How old are you, {name}? "))
+
+print(f"So you are {age} years old? Have a cake for every year I missed!")
+print(cake*age)
