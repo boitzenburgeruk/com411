43adddc049ba5ae03653a0f41ff6a9d2e6618ec4
Angus <5boita75@solent.ac.uk>
2021-10-05 17:29:10 +0100

    Added code to demonstrate the use of comparison operators

---
+++ basics/decisions/comparison_operators.py
@@ -0,0 +1,9 @@
+first_number = int(input("Please enter a number: "))
+second_number = int(input("Please enter another number: "))
+
+if first_number == second_number:
+    print("Both numbers are equal!")
+elif first_number > second_number:
+    print("The second number is smallest")
+else:
+    print("The first number is smallest")
