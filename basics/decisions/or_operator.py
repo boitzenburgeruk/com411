c843eb0eaeb6e46bde4ec8795a39890c55b90493
Angus <5boita75@solent.ac.uk>
2021-10-07 15:20:36 +0100

    Added code to demonstrate the use of a logical or operator

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,8 @@
+choice = str(input("What type of adventure should I have? "))
+
+if choice == "scary" or choice == "short":
+    print("Entering the dark forest!")
+elif choice == "safe" or choice == "long":
+    print("Taking the safe route!")
+else:
+    print("Not sure which route to take...")
