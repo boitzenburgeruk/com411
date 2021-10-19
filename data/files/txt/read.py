1c738df9f3a8179eaa7a30eca3a3f26948efb685
Angus <5boita75@solent.ac.uk>
2021-10-19 15:25:53 +0100

    Added code to demonstrate how to read a text file

---
+++ data/files/txt/read.py
@@ -0,0 +1,29 @@
+def display_chars(path, number_of_chars):
+    file = open(path)
+    output = file.read(number_of_chars)
+    print(f"The first five characters are:\n{output}")
+    file.close()
+
+
+def display_line(path):
+    file = open(path)
+    output = file.readline()
+    print(f"\nThe first line is:\n{output}")
+    file.close()
+
+
+def display_text(path):
+    file = open(path)
+    output = file.read()
+    print(f"The full text is:\n{output}")
+    file.close()
+
+
+def run():
+    path = "library.txt"
+    display_chars(path, 5)
+    display_line(path)
+    display_text(path)
+
+
+run()
