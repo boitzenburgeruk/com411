7469610d8e89b575c57f05b80d25868071dc765b
Angus <5boita75@solent.ac.uk>
2021-10-21 13:49:40 +0100

    Added code to export CSV data

--- data/files/csv/export_csv.py
+++ data/files/csv/export_csv.py
@@ -5,3 +5,18 @@ def export(path, export_number):
     print("Exporting...")
     with open(path, "a") as file:
         reader = csv.reader(file)
+        for x in range(0, export_number, 1):
+            bot_id = int(input("Please assign an ID number: "))
+            bot_name = str(input("Please enter the name for bot: "))
+            bot_paint = str(input("Please enter what colour you would like the bot: "))
+            file.write(f"\n{bot_id}, {bot_name}, {bot_paint}")
+        file.close()
+        print("Done!")
+
+
+def run():
+    export("exported_bots.csv", 2)
+
+
+if __name__ == "__main__":
+    run()
